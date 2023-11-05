001e2153449969fca91c88d4b0c26888c9238ec0
mvinodkumar9381 <116946905+mvinodkumar9381@users.noreply.github.com>
2023-11-05 09:23:08 +0530

    Initial commit

---
+++ music/musicbeats/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    
+    path('songs/',views.songs,name='songs'),
+    path('songs/<int:id>',views.songpost,name='songpost'),
+    path('login',views.login,name='login'),
+    path('signup',views.signup,name='signup'),
+    path('logout_user',views.logout_user,name='logout_user'),
+    path('watchlater',views.watchlater,name='watchlater'),
+    path('search/', views.search_songs, name='song_search'),
+    path('history/', views.history, name='history'),
+    path('c/<str:channel>', views.channel, name='channel'),
+    path('upload', views.upload, name='upload'),
+
+    
+]
